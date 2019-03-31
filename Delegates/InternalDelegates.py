8a0e1b87fe65ad1840f8682082b63f2b1f9494a4
Thy-Vipe <leo.coulombier@gmail.com>
2019-03-31 01:33:28 +0100

    Delegates are good to go, connections work

--- Delegates/InternalDelegates.py
+++ Delegates/InternalDelegates.py
@@ -1,5 +1,6 @@
 from Nodes import Core, CoreUtils
 
+
 class BoundMethod:
     def __init__(self, owner, o, fname, fRef):
         self.__Owner = owner
@@ -8,7 +9,7 @@ class BoundMethod:
         self.__FuncName = fname
 
     def call(self, *args, **kwargs):
-        self.__FuncRef(*args, **kwargs)
+        return self.__FuncRef(*args, **kwargs)
 
     def getFuncName(self):
         return self.__FuncName
@@ -27,7 +28,7 @@ class Delegate(Core.NObject):
     """
     def __init__(self, name, Owner=None):
         super(Delegate, self).__init__(name, Owner)
-        self.__functions = []
+        self._functions = []
 
     def bindFunction(self, *args):
         """
@@ -40,12 +41,12 @@ class Delegate(Core.NObject):
         if callable(args[0]):
             owningClass = CoreUtils.UCoreUtils.get_class_that_defined_method(args[0])
             name = args[0].__name__
-            self.__functions.append(BoundMethod(self.getOwner(), owningClass, name, args[0]))
+            self._functions.append(BoundMethod(self.getOwner(), owningClass, name, args[0]))
         elif isinstance(args[0], Core.NObject):
             funcRef = getattr(args[0], args[1], None)
 
             if funcRef is not None and callable(funcRef):
-                self.__functions.append(BoundMethod(self.getOwner(), args[0], args[1], funcRef))
+                self._functions.append(BoundMethod(self.getOwner(), args[0], args[1], funcRef))
             else:
                 bError = True
         else:
@@ -63,7 +64,7 @@ class Delegate(Core.NObject):
         bError = False
         BoundFunc = self.findFunc(*args)
         if BoundFunc is not None:
-            self.__functions.remove(BoundFunc)
+            self._functions.remove(BoundFunc)
         else:
             bError = True
 
@@ -71,14 +72,16 @@ class Delegate(Core.NObject):
             raise TypeError("{input} is not a function type or NObject reference, or the passed-in function name is not valid.".format(input=str(args[0])))
 
     def fire(self, *args, **kwargs):
-        for func in self.__functions:
+        for func in self._functions:
             func.call(*args, **kwargs)
 
+        return
+
     def getBoundFunctions(self):
-        return self.__functions
+        return self._functions
 
     def findFunc(self, funcNameOrObj, obj=None):
-        for bm in self.__functions:
+        for bm in self._functions:
             if isinstance(funcNameOrObj, str):
                 if bm.FuncName == funcNameOrObj:
                     if obj is not None:
@@ -95,3 +98,28 @@ class Delegate(Core.NObject):
                 raise TypeError("Input param 1 is not callable and is not a string.")
 
         return None
+
+
+class Listener(Delegate):
+    
+    def bindFunction(self, *args):
+        if len(self._functions) == 0:
+            super(Listener, self).bindFunction(*args)
+        else:
+            raise Warning("{0} is already bound to a method.".format(str(self)))
+
+    def removeFunction(self, *args):
+        """
+        /!\\ Not used by this class.
+        """
+        pass
+
+    def clear(self):
+        del self._functions[0]
+
+    def fire(self, *args, **kwargs):
+        if len(self._functions) != 0:
+            return self._functions[0].call(*args, **kwargs)
+        else:
+            print(Warning("{0} was called but is not bound to any function.".format(str(self))))
+
